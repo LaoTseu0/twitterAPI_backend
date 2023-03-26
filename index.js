cdfd1fffe26fcb3f66ef692ff5004848f2647eff
digital-fak <digital-passion@outlook.fr>
2023-03-26 22:23:24 +0200

    m

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 // origin: process.env.ENV_LOCATION === "DEV" ? process.env.ENV_DEV : process.env.ENV_HEROKU,
 app.use(
   cors({
-    origin: "https://interface-api-twitter.vercel.app/",
+    origin: "*",
   })
 );
 app.use("/tweets", tweetsRoutes);
