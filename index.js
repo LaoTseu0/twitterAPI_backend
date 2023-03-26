ad096fd331af8acd6f115a47c28ac8b27e287fc0
digital-fak <digital-passion@outlook.fr>
2023-03-26 23:56:51 +0200

    m

--- index.js
+++ index.js
@@ -10,15 +10,25 @@ const port = process.env.PORT || 5500;
 
 app.use(bodyParser.json());
 // origin: process.env.ENV_LOCATION === "DEV" ? process.env.ENV_DEV : process.env.ENV_HEROKU,
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true,
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+//   })
+// );
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next()
+});
+
 app.use("/tweets", tweetsRoutes);
+app.use((req,res) => {
+  res.json({ message: "API live !"})
+})
 
 app.listen(port, () => console.log(`Server started: ${port}`));
