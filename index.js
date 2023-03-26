d4ff31dce1483363cbb9601145062d079113d242
digital-fak <digital-passion@outlook.fr>
2023-03-26 22:14:52 +0200

    m

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const port = process.env.PORT || 5500;
 app.use(bodyParser.json());
 // origin: process.env.ENV_LOCATION === "DEV" ? process.env.ENV_DEV : process.env.ENV_HEROKU,
 app.use(
-  cors()
+  cors({
+    origin: "https://interface-api-twitter.vercel.app/",
+  })
 );
 app.use("/tweets", tweetsRoutes);
 
