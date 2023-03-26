d5f9eabe2331400e1bc736976c1e1ed140ec7656
digital-fak <digital-passion@outlook.fr>
2023-03-26 05:08:53 +0200

    m

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    Headers: "Origin, ContentType, Accept, Authorization",
+    // Headers: "Origin, ContentType, Accept, Authorization",
     preflightContinue: false,
     optionsSuccessStatus: 204,
   })
